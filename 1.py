10b56415243d37dfcb6248cf0a52409f1ef1c633
arshalt <66940182+arshalt@users.noreply.github.com>
2020-06-28 12:39:24 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,23 +1,28 @@
-from math import *
 print("Please input the amount of sales")
 a = float(input())
 b = float(50000)
 c = float(75000)
 d = float(0.1)
 e = float(0.15)
-commission1 = a * float(d)
-commission2 = a * float(e)
-if a > b:
+commission1: float = a * float(d)
+commission2: float = a * float(e)
+if a < b and a < c:
+    print("Sorry, You do not meet the sales commission criteria")
+if a >= b and not (a >= c):
     print(commission1)
-    print(".^.")
+    print(" ^ ")
+    print("/ \ ")
     print(" |")
     print(" |")
     print(" |")
     print("This is with 10% commission")
-if a > c:
+if a >= c:
     print(commission2)
-    print(".^.")
-    print(" |")
-    print(" |")
-    print(" |")
+    print(" ^ ")
+    print("/ \ ")
+    print(" | ")
+    print(" | ")
+    print(" | ")
     print("This is with 15% commission")
+
+
